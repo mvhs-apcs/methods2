6e69adb55b3e25612b970e1cb955b8e673e33f19
Tim Padjen <tpadjen@gmail.com>
2015-08-26 08:40:42 -0600

    Remove progress bar from minitest reporter

--- methods_test.rb
+++ methods_test.rb
@@ -1,4 +1,7 @@
 require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 require_relative 'methods'
 
 class MethodsTest < MiniTest::Unit::TestCase
