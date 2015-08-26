9711f4071d10f8234745a65347a18b192c721e67
Tim Padjen <tpadjen@gmail.com>
2015-08-26 08:30:15 -0600

    Add monkey_trouble?

--- methods.rb
+++ methods.rb
@@ -12,4 +12,18 @@ module Methods
 	#
 	# TODO - write sleep_in?
 
+	# monkey_trouble?
+	#
+	# We have two monkeys, a and b, and the parameters 
+	# a_smile and b_smile indicate if each is smiling. 
+	# We are in trouble if they are both smiling or if 
+	# neither of them is smiling. Return true if we are in trouble.
+	#
+	# ==== Attributes 
+	# 
+	# * a_smile   - true only if monkey a is smiling
+	# * b_smile   - true only if monkey b is smiling
+	#
+	# TODO - write monkey_trouble?
+
 end
--- methods_test.rb
+++ methods_test.rb
@@ -10,4 +10,11 @@ class MethodsTest < MiniTest::Unit::TestCase
     assert_equal true, Methods.sleep_in?(true, true)
   end
 
+  def test_monkey_trouble
+    assert_equal true, Methods.monkey_trouble?(false, false)
+    assert_equal false, Methods.monkey_trouble?(true, false)
+    assert_equal false, Methods.monkey_trouble?(false, true)
+    assert_equal true, Methods.monkey_trouble?(true, true)
+  end
+
 end
