69e4a2d75675fa84d7b3d167dd3094d4e2541b90
Tim Padjen <tpadjen@gmail.com>
2015-08-26 07:58:38 -0600

    Documentation for sleep_in?

--- methods.rb
+++ methods.rb
@@ -1,5 +1,14 @@
 module Methods
 	
 	
+	# sleep_in? decides if a student should sleep in.
+	# Students sleep in if it is not a weekday or we're on vacation. 
+	#
+	# ==== Attributes 
+	# 
+	# * weekday  - true only if it is a weekday
+	# * vacation - true only if the student is on vacation
+	#
+	# TODO - write sleep_in?
 
 end
