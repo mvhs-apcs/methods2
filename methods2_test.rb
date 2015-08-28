f81180dddc5bbf5ca3d7092806e5322d85d1babd
tpadjen <tpadjen@gmail.com>
2015-08-28 12:53:53 -0600

    Update for methods2

--- methods2_test.rb
+++ methods2_test.rb
@@ -2,12 +2,12 @@ require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative 'methods'
+require_relative 'methods2'
 
 class Methods2Test < MiniTest::Test
 	def setup
 		@m = Class.new do
-     include Methods
+     include Methods2
    	end.new
 	end
 
